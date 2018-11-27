3b6726a7b89bf3e1c8035a565f3bc5bdb5b023e1
LizVillegas57 <liz.villegas88@gmail.com>
2018-11-26 19:42:34 -0600

    Adding  package and js file
    Signed-off-by: LizVillegas57 <liz.villegas88@gmail.com>

--- november26.js
+++ november26.js
@@ -5,6 +5,7 @@
     - git branch -v --> me dice que branch existen
     - git status --> me muestra los cambios o archivos nuevos
     - git add . --> archivos que quiero agregar
+    - git commit -s -m "Mensaje de lo que trabajaste" --> los pasa al area de commit, el -s le agrega mi nombre y mi correo
 
 
     Areas:

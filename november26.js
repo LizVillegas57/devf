77e2c9bf3a4345f32926b457d140808a2693fc8c
Liz <liz.villegas88@gmail.com>
2018-11-26 21:38:48 -0600

    changes in js file
    Signed-off-by: Liz <liz.villegas88@gmail.com>

--- november26.js
+++ november26.js
@@ -9,6 +9,11 @@
     - git config --global user.name "Liz" --> configurar nuestro usuario que va a usar git
     git config --global user.email "liz.villegas88@gmail.com"  --> configurar nuestro email que va a usar git
     - git checkout -b rama --> creas nuevas ramas o branch
+    - git merge --> haces una fusion con master o con cualquier brancg que quieras. 
+    - git remote add origin link --> el link es el https del repo a donde quiero subir todo
+    - git push origin master --> con push subes tus cambios al branch que quieras, pero generalmente es master
+    - git pull origin master --> con pull bajas  cambios a tu local, de cualquier master
+    - git clone link --> clono algun repo que exista
 
 
     Areas:
@@ -17,4 +22,24 @@
     - Commit 
 */
 
-console.log("CAMBIO DE BRANCH");
+
+/*
+    Bases de datos
+        - Conjunto de datos relacionados entre si
+        - Serie de datos organizados y relacionados entre si, son recolectados y explotados por los sistemas de informacion de una empresa o negocio particular.
+        Propiedades:
+            * Independencia logica y fisica
+            * Redundancia minima
+            * Acceso concurrente por parte de multiples usuarios
+            * Integridad de los datos
+            * Consultas complejas optimizadas
+            * Seguridad de acceso y auditoria
+            * Respaldo y recuperacion
+        
+        - son datos CRUDOS
+        - SQL: Structure query language, realiza consultas
+        - sql son tablas, nosql son coleccion de objetos
+        - tipos de campos: numericos, booleanos, fechas, alfanumericos, autoincrementables
+        - ORM Y ODM: MONGODB
+*/
+

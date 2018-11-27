d502eecc6021ddb0c279a669ffee3ba960538866
Liz <liz.villegas88@gmail.com>
2018-11-26 19:53:05 -0600

    Change on js file
    Signed-off-by: Liz <liz.villegas88@gmail.com>

--- november26.js
+++ november26.js
@@ -6,6 +6,9 @@
     - git status --> me muestra los cambios o archivos nuevos
     - git add . --> archivos que quiero agregar
     - git commit -s -m "Mensaje de lo que trabajaste" --> los pasa al area de commit, el -s le agrega mi nombre y mi correo
+    - git config --global user.name "Liz" --> configurar nuestro usuario que va a usar git
+    git config --global user.email "liz.villegas88@gmail.com"  --> configurar nuestro email que va a usar git
+    - git checkout -b rama --> creas nuevas ramas o branch
 
 
     Areas:
@@ -13,3 +16,5 @@
     - Staging --> area intermedia 
     - Commit 
 */
+
+console.log("CAMBIO DE BRANCH");

ad5fed6b61305f34793c51f24355c018863e23a8
LizVillegas57 <liz.villegas88@gmail.com>
2018-11-26 19:41:21 -0600

    Adding  package and js file
    Signed-off-by: LizVillegas57 <liz.villegas88@gmail.com>

---
+++ november26.js
@@ -0,0 +1,14 @@
+/* Git
+    - Nunca se toca el master por que es produccion
+    
+    Comandos:
+    - git branch -v --> me dice que branch existen
+    - git status --> me muestra los cambios o archivos nuevos
+    - git add . --> archivos que quiero agregar
+
+
+    Areas:
+    - Working --> area local
+    - Staging --> area intermedia 
+    - Commit 
+*/
